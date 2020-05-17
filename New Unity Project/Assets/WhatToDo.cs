f446a384a921ef6fb32c5f344493e85693598f54
BlueAmDK <a1225485723@163.com>
2020-05-17 16:32:51 +0800

    Signed-off-by: BlueAmDK <a1225485723@163.com> testforunity 0517

--- New Unity Project/Assets/WhatToDo.cs
+++ New Unity Project/Assets/WhatToDo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WhatToDo : MonoBehaviour
 {
+    public int testtxt = 123;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,12 @@ public class WhatToDo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(testtxt>=123)
+        {
+            //test1 20200517 makefor ZYP
+            print("test1");
+            testtxt -= 1;
+        }
         
     }
 }
